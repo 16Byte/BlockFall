79162f8d32b57f17b6573df2952c2a18063f0c3f
16Byte <pixeldevs.diego@gmail.com>
2015-12-10 08:53:58 -0700

    Added main files

---
+++ Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public GameObject LeftButton;
+	public GameObject RightButton;
+	public GameObject Player;
+
+	public bool isLeft;
+	public bool isRight;
+
+	public int Speed;
+	public int RotationSpeed; //Even though we don't need rotation lol
+
+	void OnMouseDown() {
+		while (isLeft) {
+			/*if (isLeft || Input.GetKeyDown (KeyCode.A)) {*/
+				GetComponent<Renderer> ().material.color = Color.blue;
+				MoveLeft ();
+			//} 
+		}
+
+		if (isRight /*|| Input.GetKeyDown(KeyCode.D)*/) {
+			GetComponent<Renderer> ().material.color = Color.blue;
+			MoveRight();
+		} 
+
+	}
+
+	void OnMouseUp() {
+		if (isLeft) {
+			GetComponent<Renderer> ().material.color = Color.white;
+		}
+
+		if (isRight) {
+			GetComponent<Renderer> ().material.color = Color.white;
+		}
+	}
+
+	void MoveLeft() {
+		Player.GetComponent<Rigidbody> ().transform.Translate (Vector3.left * Speed * Time.deltaTime, Space.World);
+	}
+
+	void MoveRight() {
+
+	}
+
+}
